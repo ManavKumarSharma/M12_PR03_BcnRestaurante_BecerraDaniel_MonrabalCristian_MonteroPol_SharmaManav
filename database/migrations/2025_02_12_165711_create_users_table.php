103e908d8fc9a735464cd119138ba869096c023f
Christian <christianmonrabaldonis@gmail.com>
2025-02-12 19:14:21 +0100

    add sessions

--- database/migrations/2025_02_12_165711_create_users_table.php
+++ database/migrations/2025_02_12_165711_create_users_table.php
@@ -18,11 +18,20 @@ return new class extends Migration {
 
             $table->foreign('id_rol')->references('id_rol')->on('roles');
         });
+
+        Schema::create('sessions', function (Blueprint $table) {
+            $table->string('id')->primary();
+            $table->foreignId('user_id')->nullable()->index();
+            $table->string('ip_address', 45)->nullable();
+            $table->text('user_agent')->nullable();
+            $table->longText('payload');
+            $table->integer('last_activity')->index();
+        });
     }
-    
 
     public function down()
     {
+        Schema::dropIfExists('sessions');
         Schema::dropIfExists('users');
     }
 };
