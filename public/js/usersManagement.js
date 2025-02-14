0d5827fe0909367d8e253cdf147b0f3fec0ff1f3
Manav Kumar Sharma <100007272.joan23@fje.edu>
2025-02-14 15:44:17 +0100

    Primera versión página admin, layout realizado

---
+++ public/js/usersManagement.js
@@ -0,0 +1,37 @@
+// Función que carga la tabla
+function loadTable() {
+    getUsersFromAPIAsync()
+        .then(users => {
+            console.log(users);
+        })
+        .catch(error => {
+            console.error("Error al cargar los usuarios:", error);
+        });
+}
+
+// Función para obtener los usuarios desde la API usando fetch
+function getUsersFromAPIAsync() {
+    return fetch('http://127.0.0.1:8000/api/users/list')
+        .then(response => {
+            if (!response.ok) {
+                throw new Error("Error en la respuesta de la API");
+            }
+            return response.json(); // Convierte la respuesta a JSON
+        })
+        .catch(error => {
+            console.error("Error al obtener los usuarios:", error);
+            return []; // Retorna un array vacío en caso de error
+        });
+}
+
+
+// Función de carga
+window.onload = function() {
+    // Ocultamos el loader
+    document.getElementById('loader').style.display = 'none';
+    // Mostramos el contenido
+    document.getElementById('main').style.display = 'block';
+
+    // Llamamos a loadTable para cargar la tabla
+    loadTable();
+};
--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,16 @@
 <?php
 
+use App\Http\Controllers\RestaurantController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
-Route::get('/', function () {
-    return view('index');
+// Rutas para el UserController
+Route::controller(UserController::class)->group(function () {
+    Route::get('/admin/users', 'showUsersAdminView')->name('admin.users'); // Endpoint de CRUD usuarios
+    Route::get('/api/users/list', 'getAllUsersFromDB'); // Endopoint para obtener todos los usuarios con su rol en JSON
 });
 
+// Rutas para el RestaurantController
+Route::controller(RestaurantController::class)->group(function () {
+    Route::get('/admin/restaurants', 'showRestaurantsAdminView')->name('admin.restaurants'); // Endpoint de CRUD restaurantes
+});
